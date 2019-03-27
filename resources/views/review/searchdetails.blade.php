4caac931c639a0b7d6a51cc9ffa03c41c49d6706
George <Stupidtown@hotmail.co.uk>
2019-03-27 23:35:27 +0000

    Updated hyperlink to be the same as others.

--- resources/views/review/searchdetails.blade.php
+++ resources/views/review/searchdetails.blade.php
@@ -16,11 +16,13 @@
 <h6><u>Ordered by recently updated:</u></h6>
 
 @foreach ($reviewSearch as $result)
+<a id="hyperlink" href="{{url('details/'.$result->id)}}">
 <div class="alert alert-info" role="alert">
-<p>{{\Carbon\Carbon::parse($result->created_at)->format('d/m/Y')}} - <a id="hyperlink" href="{{url('details/'.$result->id)}}">{{$result->review_title}}</a></p>
+<p>{{\Carbon\Carbon::parse($result->created_at)->format('d/m/Y')}} - {{$result->review_title}}</p>
 <p>Review by: {{$result->review_by}}</p>
 <p>Game reviewed: {{$result->game_title}}</p>
 </div>
+</a>
 @endforeach
 {{ $reviewSearch->appends(Request::except('page'))->links('vendor.pagination.bootstrap-4') }}
 @else
